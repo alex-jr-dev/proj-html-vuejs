efbf7c525d0fd9f0371448b213f679e8e719c30e
alex-jr-dev <geremia.alessandro@icloud.com>
2021-05-24 22:40:23 +0200

    Implemented ul with vue

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+new Vue({
+  el: "#app",
+  data: {
+    headerlinks: [
+      {
+        text: "Home",
+        span: "",
+      },
+      {
+        text: "Who We Are",
+        span: "",
+      },
+      {
+        text: "What We Do",
+        span: "",
+      },
+      {
+        text: "Where We Work",
+        span: "",
+      },
+      {
+        text: "Careers",
+        span: "Apply",
+      },
+      {
+        text: "News",
+        span: "",
+      },
+    ],
+    footerlinks: [
+      {
+        text: "Home",
+      },
+      {
+        text: "Who We Are",
+      },
+      {
+        text: "What We Do",
+      },
+      {
+        text: "Where We Work",
+      },
+      {
+        text: "Careers",
+      },
+      {
+        text: "News",
+      },
+      {
+        text: "Get in touch now",
+      },
+    ],
+  },
+});
